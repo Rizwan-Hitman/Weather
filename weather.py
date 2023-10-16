13ca2def204a9a9d2ea0c5047ec7b99b15b2066d
Rizwan <rizwanmunir190@gmail.com>
2023-10-16 19:21:13 +0500

    second

--- weather.py
+++ weather.py
@@ -13,17 +13,17 @@ def get_current_weather(city):
     weather_data = requests.get(request_url).json()
     return weather_data
 
-# if __name__ == "__main__":
-#     print("\n***Get Current Weather Condition ***\n")
+if __name__ == "__main__":
+    print("\n***Get Current Weather Condition ***\n")
 
 
-#     city= input("\nPlease enter a city name:")
+    city= input("\nPlease enter a city name:")
 
-#     if not bool(city.strip()):
-#         city = "Kansas City"
+    if not bool(city.strip()):
+        city = "Kansas City"
 
 
-#     weather_data = get_current_weather(city)
-
-#     print("\n")
-#     pprint(weather_data)
+    weather_data = get_current_weather
+    
+    print("\n")
+    pprint(weather_data(city))
